4808f3f62723e0d0fb93668871b129c0ffb69531
Nawaraj Pudasaini <active99raj@gmail.com>
2026-02-04 14:12:46 +1100

    request args for get and req.form.get for post

--- testpy/README.md
+++ testpy/README.md
@@ -7,3 +7,10 @@ flask run
 
 render_template for templates like index.html import
 url_for for css static files
+
+# To open on another machine or after clone
+python -m venv testpy
+source testpy/bin/activate 
+
+pip install flask
+flask run
--- testpy/app.py
+++ testpy/app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+# with app.test_request_context():
+#     print(url_for('index'))
+
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html")
-with app.test_request_context():
-    print(url_for('index'))
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
+    
+# def greet():
+#     return render_template("greet.html", name=request.args.get("name", "world"))
+
+@app.route("/register", methods=["GET","POST"])    
+def register():
+    return render_template("success.html", name=request.args.get("name", "world"))
